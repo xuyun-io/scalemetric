11ae4a62d6197cbdf9ec779cf77d1ea3bac9d363
yulibaozi <yulibaozi@qq.com>
2020-09-11 10:27:04 +0800

    fix: update metrics

--- metrics.go
+++ metrics.go
@@ -42,44 +42,7 @@ func Metric(pod *v1.Pod) {
 	fmt.Println(status)
 	fmt.Println("各节点预计未来不可调度原因: ")
 	for _, nodesche := range schedulerStatus.NodeScheduling {
-
-		// nodeSchedulingStatus := clusterStatus.SchedulingStatus[0].NodeSchedulingStatus[i]
 		lastR := fmt.Sprintf("     节点: %s, 还能调度: %d, 原因: %s", nodesche.Node.GetName(), nodesche.PredMaxschedulingCount, nodesche.LastReason[0].Error())
 		fmt.Println(lastR)
 	}
-	panic("xxx")
-	// var (
-	// 	count               int64
-	// 	podSchedulingStatus = PodSchedulingStatus{}
-	// )
-
-	// podSchedulingStatus.Pod = pod
-
-	// for i := range nodes.Items {
-	// 	nodePods := resources.FilterNodePods(&nodes.Items[i], pods)
-	// 	max, LastReason, err := calculate.NodeMaxScheduling(&nodes.Items[i], nodePods, pod)
-	// 	if err != nil {
-	// 		panic(err.Error())
-	// 	}
-	// 	count = count + max
-
-	// 	podSchedulingStatus.NodeSchedulingStatus = append(podSchedulingStatus.NodeSchedulingStatus, NodeSchedulingStatus{
-	// 		NodeName:               nodes.Items[i].GetName(),
-	// 		PredMaxschedulingCount: max,
-	// 		LastReason:             LastReason,
-	// 	})
-
-	// }
-	// podSchedulingStatus.PredMaxschedulingCount = count
-	// clusterStatus := ClusterSchedulingStatus{
-	// 	SchedulingStatus: []PodSchedulingStatus{podSchedulingStatus},
-	// }
-	// status := fmt.Sprintf("预计集群还能调度(1c2g)Pod数: %d", clusterStatus.SchedulingStatus[0].PredMaxschedulingCount)
-	// fmt.Println(status)
-	// fmt.Println("各节点预计未来不可调度原因: ")
-	// for i := range clusterStatus.SchedulingStatus[0].NodeSchedulingStatus {
-	// 	nodeSchedulingStatus := clusterStatus.SchedulingStatus[0].NodeSchedulingStatus[i]
-	// 	lastR := fmt.Sprintf("     节点: %s, 还能调度: %d, 原因: %s", nodeSchedulingStatus.NodeName, nodeSchedulingStatus.PredMaxschedulingCount, nodeSchedulingStatus.LastReason[0])
-	// 	fmt.Println(lastR)
-	// }
 }
