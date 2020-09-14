4b4ee65fb68529f529cb54aa91d8cafcd4bb03fa
yulibaozi <yulibaozi@qq.com>
2020-09-14 10:40:49 +0800

    feat: add curl exec

---
+++ exec/crul.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -X POST -d @eg-pod.json http://host:8080/api/cluster/scalemetric
---
+++ exec/curl.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -X POST -d @eg-pod.json http://a99ec03d1f28311eabd030ae00c3ed4a-1498960814.ap-northeast-1.elb.amazonaws.com:8080/api/cluster/scalemetric
